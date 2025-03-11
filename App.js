9b53d375f920f92edb60c17227ae216ce26aa7f5
nikgithub05 <147246952+nikgithub05@users.noreply.github.com>
2025-03-11 20:35:21 +0530

    App.js
    there might be an issue with the path for TripPlan.jsx file

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AuthForm from './components/AuthForm';
 import Dashboard from './components/dashboard';
+import TripPlan from './components/TripPlan';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<AuthForm />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/trip-plan" element={<TripPlan />} />
     </Routes>
   );
 };
